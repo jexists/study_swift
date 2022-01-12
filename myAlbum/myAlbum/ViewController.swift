3a56144be95fc39d4fbac8a8c0e5c00fb945b335
jexists <jexists486@gmail.com>
2022-01-12 22:03:50 +0900

    alert 팝업 띄우기

--- myAlbum/myAlbum/ViewController.swift
+++ myAlbum/myAlbum/ViewController.swift
@@ -14,6 +14,15 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func hello(_ sender: Any) {
+//        print("hello")
+        let alert = UIAlertController(title: "Hello", message: "my first app!", preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
 }
 
